11d4ee7476eb4326548cb4f960c4b31cd8152dbb
poomhello2549 <poomhello2549@gmail.com>
2026-04-01 02:19:22 +0700

    Update lab24_3.cpp

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
-
+#include<set>
 using namespace std;
 
 //Write function count() here.
+int count(int data[],int size){
+    set<int> setnum;
+    int sum = 0;
+    for(int i=0;i<size;i++){
+        setnum.insert(data[i]);
+    }
+    set<int>::iterator i;
+    for(i=setnum.begin();i != setnum.end();i++){
+        sum++;
+    }
+    return sum;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
